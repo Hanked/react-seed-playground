77cdd80aa5c54c904a1415f44fa1d7e164331cd7
Hanked <dave.kingsnorth@papershaker.com>
2015-10-14 01:38:39 +0100

    example firebase data sync with re-base

--- app/components/Auth/Auth.jsx
+++ app/components/Auth/Auth.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
+// var Firebase = require("firebase");
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://boiling-fire-6508.firebaseio.com');
+
+const uuid = "aa9001b74klq002";
 
 let { Component, PropTypes } = React;
 
 export default class Auth extends Component {
-  constructor(){
-    super()
-    this.state = {}
+  constructor(props){
+    super(props);
+    this.state = {
+      users: {}
+    };
+  }
+
+  componentDidMount(){
+    this.ref = base.syncState('users', {
+      context: this,
+      state: 'users'
+    });
+  }
+
+  componentWillUnmount(){
+    base.removeBinding(this.ref);
   }
 
   render(){
-    return (
-      <div>
-        <h2>Auth</h2>
-        {this.props.children}
-      </div>
-    )
+    console.log(typeof this.state.users);
+    console.log(this.state.users[uuid]);
+
+    if (this.state.users.hasOwnProperty(uuid)) {
+      return (
+        <div>
+          <h2>{this.state.users[uuid].firstName} {this.state.users[uuid].lastName}</h2>
+          <h3>{this.state.users[uuid].dob}</h3>
+          {this.props.children}
+        </div>
+      )
+    } else {
+      return (
+        <div></div>
+      )
+    }
+
+
   }
 }
